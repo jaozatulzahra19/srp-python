646b0a2649a2093851bda3da2003fc8b83ac0f1d
Mila3010 <milakarmeliyah45@gmail.com>
2022-05-23 23:10:27 +0700

    perubahan balok view

---
+++ contoh/main_balok.py
@@ -0,0 +1,10 @@
+from balok import Balok    
+from balok_controller import BalokController    
+from balok_view import BalokView 
+
+balok = Balok(2)
+penghitung_balok = BalokController()
+penampil_balok = BalokView()  
+
+penampil_balok.show_luas(balok, penghitung_balok)
+penampil_balok.show_volume(balok, penghitung_balok)
