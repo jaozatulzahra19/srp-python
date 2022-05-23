ad092511a1ff5526aaac08c37531157eb2413946
jaozatulzahra19 <jaozatulzahra19@gmail.com>
2022-05-23 22:31:28 +0700

    perubahan balok

---
+++ latihan/balok_controller.py
@@ -0,0 +1,15 @@
+from balok import Balok
+
+class BalokController:
+    
+    def hitung_panjang(self, panjang: Panjang) -> float:
+        return balok.get_panjang() * balok.get_panjang()
+    
+    def hitung_lebar(self, lebar: Lebar) -> float:
+        return balok.get_lebar() * balok.get_lebar()
+    
+    def hitung_lebar(self, lebar: Lebar) -> float:
+        return balok.get_lebar() * balok.get_lebar()
+    
+    def hitung_keliling(self, balok: Balok) -> float:
+        return 4 * persegi.get_sisi()
